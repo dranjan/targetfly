56990d7fd35a07be1d099c80dadc39760c2fb781
Darsh Ranjan <dranjan@math.berkeley.edu>
2013-09-29 04:21:13 -0700

    Add file size and mtime to directory listing

--- index.js
+++ index.js
@@ -36,8 +36,80 @@ app.locals({
     version: version,
     platform: process.platform,
     arch: process.arch,
-    node_version: process.version
-})
+    node_version: process.version,
+    formatSize: formatSize,
+    formatDateFull: formatDateFull
+});
+
+function formatSize(n) {
+    function fmt(e, c) {
+        var m = n/Math.pow(2, e);
+
+        var precision = 0;
+        if (m < 10) precision = 2;
+        else if (m < 100) precision = 1;
+
+        return m.toFixed(precision) + c;
+    }
+
+    if (n < Math.pow(2, 10)) {
+        return String(n);
+    } else if (n < Math.pow(2, 20)) {
+        return fmt(10, "K");
+    } else if (n < Math.pow(2, 30)) {
+        return fmt(20, "M");
+    } else if (n < Math.pow(2, 40)) {
+        return fmt(30, "G");
+    } else if (n < Math.pow(2, 50)) {
+        return fmt(40, "T");
+    } else {
+        return fmt(50, "P");
+    }
+}
+
+function formatDateFull(d, d0) {
+    function formatYear(d, d0) {
+        if (d.getFullYear() !== d0.getFullYear()) {
+            return String(d.getFullYear()) + '&nbsp;&nbsp;';
+        } else {
+            return '';
+        }
+    }
+
+    function formatMonth(d, d0) {
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+        if (d.getFullYear() !== d0.getFullYear() ||
+            d.getMonth() !== d0.getMonth() ||
+            d.getDate() !== d0.getDate())
+        {
+            return months[d.getMonth()] + '&nbsp;';
+        } else {
+            return '';
+        }
+    }
+
+    function formatDate(d, d0) {
+        if (d.getFullYear() !== d0.getFullYear() ||
+            d.getMonth() !== d0.getMonth() ||
+            d.getDate() !== d0.getDate())
+        {
+            var s = String(d.getDate());
+            if (s.length == 1) s = '&nbsp;' + s;
+            return s + ',&nbsp;';
+        } else {
+            return '';
+        }
+    }
+
+    function formatTime(d, d0) {
+        return d.toTimeString();
+    }
+
+    return formatYear(d, d0) + formatMonth(d, d0) +
+           formatDate(d, d0) + formatTime(d, d0);
+}
 
 app.use(express.logger('dev'));
 
@@ -102,7 +174,8 @@ app.get(/^\/browse\/(.*)/, function (request, response) {
                     genericError(response, err);
                 } else {
                     response.render('directory', 
-                                    {files: files, path: dirpath});
+                                    {files: files, path: dirpath,
+                                     date: new Date()});
                 }
             });
         } else {
@@ -157,12 +230,12 @@ function dirInfo(dirpath, callback) {
             callback(null, filename);
         };
 
-        tasks.stat = function (callback) {
-            isDir(filepath, function (err, v) {
+        tasks.stats = function (callback) {
+            fs.stat(filepath, function (err, stats) {
                 if (err) {
                     callback(null, null);
                 } else {
-                    callback(null, {isdir: v});
+                    callback(null, stats);
                 }
             });
         };
@@ -184,21 +257,6 @@ function dirInfo(dirpath, callback) {
             function (files) {
                 async.map(files, fileInfo, callback);
             });
-        }
-    });
-}
-
-/* callback is callback(err, <boolean>).  It will be called once with
- * the <boolean> representing whether the filename is a directory or
- * not.  err will be the error from fs.stat (if any).
- */
-function isDir(filename, callback) {
-    fs.stat(filename, function (err, stats) {
-        if (err) {
-            callback(err);
-        }
-        else {
-            callback(null, stats.isDirectory());
         }
     });
 }
