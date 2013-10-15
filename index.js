76548fc0403e66e3245c3c20173b088f288eb889
Darsh Ranjan <dranjan@math.berkeley.edu>
2013-10-14 22:12:49 -0700

    Remove express dependency
    This reduces targetfly's dependency footprint by a bit.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var http = require('http');
+var url = require('url');
+
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 var jade = require('jade');
 var program = require('commander');
 var inspect = require('util').inspect;
 var path = require('path');
-var http = require('http');
 var async = require('async');
 var mime = require('mime');
 
@@ -36,24 +37,114 @@ var showHidden = program.showHidden;
 var showBackup = program.showBackup;
 var tar = program.tar;
 
-var app = express();
-
+var views = path.join(__dirname, 'views');
 var meta = path.join(__dirname, "meta");
 
-app.locals({
-    version: version,
-    platform: process.platform,
-    arch: process.arch,
-    node_version: process.version,
-    formatSize: formatting.formatSize,
-    formatDateFull: formatting.formatDateFull
-});
+function serve(port) {
+    function onRequest(request, response) {
+        console.log(request.connection.remoteAddress + ':' +
+                    request.connection.remotePort + " " +
+                    request.method + " " + request.url);
 
-app.use(express.logger('dev'));
+        var requestUrl = url.parse(request.url);
+        var pathname = requestUrl.pathname;
 
-app.set('views', __dirname + '/views');
-app.engine('.jade', jade.renderFile);
-app.set('view engine', 'jade');
+        if (redirects[pathname]) {
+            response.redirect(redirects[pathname]);
+        } else {
+            var components = pathname.split('/');
+            var handler = routes[components[1]];
+            if (handler) {
+                handler(components.slice(2).join('/'), response);
+            } else {
+                response.error404();
+            }
+        }
+    }
+
+    http.createServer(onRequest).listen(port);
+    console.log("Serving " + root + " on port " + String(port) + "...");
+}
+
+http.ServerResponse.prototype.render = function (view, locals) {
+    var renderLocals = {
+        version: version,
+        platform: process.platform,
+        arch: process.arch,
+        node_version: process.version,
+        formatSize: formatting.formatSize,
+        formatDateFull: formatting.formatDateFull
+    };
+
+    for (var k in locals) {
+        renderLocals[k] = locals[k];
+    }
+
+    var res = this;
+    function writeHtml(err, html) {
+        if (err) {
+            res.status = 500;
+            console.log(err);
+            res.end();
+        } else {
+            res.status = 200;
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': html.length
+            });
+            res.end(html);
+        }
+    };
+
+    jade.renderFile(path.join(views, view + '.jade'),
+                    renderLocals,
+                    writeHtml);
+};
+
+http.ServerResponse.prototype.redirect = function (location) {
+    this.writeHead(302, {'Location': location});
+    this.end();
+};
+
+http.ServerResponse.prototype.genericError = function (err) {
+    var errStr = inspect(err);
+    console.log(errStr);
+    var s = err.status || 500;
+    if (s === 404) {
+        this.error404();
+    } else {
+        this.statusCode = s;
+        this.render('error', {code:s, message:errStr});
+    }
+};
+
+http.ServerResponse.prototype.error404 = function () {
+    this.statusCode = 404;
+    this.render('404');
+};
+
+function httpError(code) {
+    var err = new Error(http.STATUS_CODES[code]);
+    err.status = code;
+    return err;
+};
+
+var redirects = {
+    '/browse': '/browse/',
+    '/download': '/download/',
+    '/': '/browse/'
+};
+
+function validatePath(pathname) {
+    var components = pathname.split('/');
+
+    for (var i in components) {
+        if (exclude_component(components[i])) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
 function exclude_component(f) {
@@ -66,111 +157,109 @@ function exclude_component(f) {
     return false;
 }
 
-app.all('*', function (request, response, next) {
-    var pathname = request.path;
-    var components = pathname.split('/');
-    var valid = true;
-
-    for (var i in components) {
-        if (exclude_component(components[i])) {
-            valid = false;
-            error404(response);
+routes = {
+    'browse': function (pathname, response) {
+        if (!validatePath(pathname)) {
+            response.error404();
+            return;
         }
-    }
 
-    if (valid) next();
-});
-
-app.get('/', function (request, response) {
-    response.redirect('/browse/');
-});
-
-app.get(/^\/browse$/, function(request, response) {
-    response.redirect('/browse/');
-});
-
-app.get(/^\/download$/, function(request, response) {
-    response.redirect('/download/');
-});
-
-app.get(/^\/browse\/(.*)/, function (request, response) {
-    var pathname = path.resolve(root, request.params[0]);
-    fs.stat(pathname, function (err, stats) {
-        if (err) {
-            error404(response);
-        } else if (stats.isDirectory()) {
-            var dirpath = '/' + request.params[0];
-            if (dirpath[dirpath.length-1] !== '/') {
-                dirpath = dirpath + '/';
-            }
-
-            dirInfo(pathname, function (err, files) {
-                if (err) {
-                    genericError(response, err);
-                } else {
-                    response.render('directory', 
-                                    {files: files, path: dirpath,
-                                     date: new Date()});
+        var truePath = path.resolve(root, pathname);
+        fs.stat(truePath, function (err, stats) {
+            if (err) {
+                response.error404();
+            } else if (stats.isDirectory()) {
+                var dirpath = '/' + pathname;
+                if (dirpath[dirpath.length-1] !== '/') {
+                    dirpath = dirpath + '/';
                 }
-            });
-        } else {
-            sendFile(pathname, response);
-        }
-    });
-});
 
-app.get(/^\/download\/(.*)/, function(request, response) {
-    var pathname = path.resolve(root, request.params[0]);
-    fs.stat(pathname, function (err, stats) {
-        if (err) {
-            error404(response);
-        } else if (stats.isDirectory()) {
-            sendTar(pathname, response);
-        } else {
-            sendFile(pathname, response, true);
-        }
-    });
-});
-
-app.get(/^\/meta\/(.*)/, function(request, response) {
-    var pathname = path.resolve(meta, request.params[0]);
-    fs.stat(pathname, function(err, stats) {
-        if (err) {
-            error404(response);
-        } else if (stats.isDirectory()) {
-            genericError(response, httpError(403));
-        } else {
-            sendFile(pathname, response);
-        }
-    });
-});
-
-
-app.get(/^\/measure\/(.*)/, function (request, response) {
-    var pathname = path.resolve(root, request.params[0]);
-    recursiveSize(pathname, function (dirname, filename) {
-        return !exclude_component(filename);
+                dirInfo(truePath, function (err, files) {
+                    if (err) {
+                        response.genericError(err);
+                    } else {
+                        response.render('directory', {
+                            files: files,
+                            path: dirpath,
+                            date: new Date()
+                        });
+                    }
+                });
+            } else {
+                sendFile(truePath, response);
+            }
+        });
     },
 
-    function (err, sz, nf, nd) {
-        if (err) {
-            error404(response);
-        } else {
-            response.status(200);
-            response.type('application/json');
-            response.end(JSON.stringify({
-                size : sz,
-                numFiles: nf,
-                numDirectories: nd
-            }));
+    'download': function (pathname, response) {
+        if (!validatePath(pathname)) {
+            response.error404();
+            return;
         }
-    });
-});
 
+        var truePath = path.resolve(root, pathname);
+        fs.stat(truePath, function (err, stats) {
+            if (err) {
+                response.error404();
+            } else if (stats.isDirectory()) {
+                sendTar(truePath, response);
+            } else {
+                sendFile(truePath, response, true);
+            }
+        });
+    },
 
-app.use(function (request, response) {
-    error404(response);
-});
+    'meta': function (pathname, response) {
+        var components = pathname.split('/');
+        for (var i in components) {
+            c = components[i];
+            if (c[0] === '.' ||
+                c[0] === '#' || c[c.length-1] === '~')
+            {
+                response.error404();
+            }
+        }
+
+        var truePath = path.resolve(meta, pathname);
+
+        fs.stat(truePath, function(err, stats) {
+            if (err) {
+                response.error404();
+            } else if (stats.isDirectory()) {
+                response.genericError(httpError(403));
+            } else {
+                sendFile(truePath, response);
+            }
+        });
+    },
+
+    'measure': function (pathname, response) {
+        if (!validatePath(pathname)) {
+            response.error404();
+            return;
+        }
+
+        var truePath = path.resolve(root, pathname);
+        recursiveSize(truePath, function (dirname, filename) {
+            return !exclude_component(filename);
+        },
+        function (err, sz, nf, nd) {
+            if (err) {
+                response.error404();
+            } else {
+                response.writeHead(200, {
+                    'Content-Type': 'application/json'
+                });
+                response.end(JSON.stringify({
+                    size : sz,
+                    numFiles: nf,
+                    numDirectories: nd
+                }));
+            }
+        });
+    }
+};
+
 
 /* callback is callback(err, files), where files is an array whose
  * entries are {name:filename, stat:stats}.  The 'stat' field can be
@@ -236,23 +325,22 @@ function sendFile(filepath, response, attach) {
     try {
         var file = fs.createReadStream(filepath);
     } catch (err) {
-        error404(response);
+        reponse.error404();
     }
 
-    response.status(200);
-    response.type(mime.lookup(filepath));
+    response.statusCode = 200;
+    response.setHeader('Content-Type', mime.lookup(filepath));
 
     if (attach) {
-        response.set({
-            "Content-Disposition": "attachment; filename=" + filename
-        });
+        response.setHeader("Content-Disposition",
+                           "attachment; filename=" + filename);
     }
 
     file.on('error', function (err) {
         if (!response.headerSent) {
             response.removeHeader("Content-Type");
             response.removeHeader("Content-Disposition");
-            genericError(response, httpError(500));
+            response.genericError(httpError(500));
         }
     });
 
@@ -283,11 +371,10 @@ function sendTar(dirpath, response) {
     try {
         var child = spawn(tar, tar_opts);
     } catch (err) {
-        genericError(response, httpError(500));
+        response.genericError(httpError(500));
     }
 
-    response.status(200);
-    response.set({
+    response.writeHead(200, {
         "Content-Type": "application/octet-stream",
         "Content-Disposition": "attachment; filename=" + tarname
     });
@@ -296,7 +383,7 @@ function sendTar(dirpath, response) {
         if (!response.headerSent) {
             response.removeHeader("Content-Type");
             response.removeHeader("Content-Disposition");
-            genericError(response, httpError(500));
+            response.genericError(httpError(500));
         }
     }
 
@@ -308,23 +395,4 @@ function sendTar(dirpath, response) {
     child.stdout.pipe(response);
 }
 
-function genericError(response, err) {
-    var errStr = inspect(err);
-    console.log(errStr);
-    var s = err.status || 500;
-    if (s === 404) error404(response);
-    else response.status(s).render('error', {code:s, message:errStr});
-}
-
-function error404(response) {
-    response.status(404).render('404');
-}
-
-function httpError(code) {
-    var err = new Error(http.STATUS_CODES[code]);
-    err.status = code;
-    return err;
-}
-
-app.listen(port);
-console.log("Serving " + root + " on port " + String(port) + "...");
+serve(port);
